11ab080f318a2a393cc59b48db2187fb6767ec2e
Nelson Castillo <ncastillo@ebclosion.com>
2022-06-02 15:49:35 -0400

    [NC ADD] Dirección de organización en el resumen del form en doctrine y entity

--- src/AppBundle/Entity/Organization.php
+++ src/AppBundle/Entity/Organization.php
@@ -20,6 +20,11 @@ class Organization
     private $name;
 
     /**
+     * @var string
+     */
+    private $address;    
+
+    /**
      * @var \DateTime
      */
     private $createdAt;
@@ -75,6 +80,29 @@ class Organization
     public function getName()
     {
         return $this->name;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     * @return Organization
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string 
+     */
+    public function getAddress()
+    {
+        return $this->address;
     }
 
     /**
